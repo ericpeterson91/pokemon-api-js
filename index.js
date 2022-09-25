af50ac41bd8cf7c07637e24f4e63ed6267fabf34
Eric Peterson <ericpeterson91@live.ca>
2022-09-25 17:42:10 -0600

    fetch data

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const fetchPokemon = () => {
+    const url = `https://pokeapi.co/api/v2/pokemon/` 
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
     
 }
+
+fetchPokemon()

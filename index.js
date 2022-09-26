3ccc0322ccf936e9d162ede992b93ec22627b394
Eric Peterson <ericpeterson91@live.ca>
2022-09-25 23:45:01 -0600

    fetch data all into one promise

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/` 
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    
+  const promises = [];
+
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites["front_default"],
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    displayPokemon(pokemon)
+  });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon)
 }
 
-fetchPokemon()
+
+fetchPokemon();
